71c1a76bc5eea1363e4ffdfa50bcd2f5ae3c5e06
SahibaSaini <159014350+SahibaSaini@users.noreply.github.com>
2024-02-28 16:14:17 +0530

    Delete sahiba96.c

--- sahiba96.c
+++
@@ -1,9 +0,0 @@
-#include<math.h>
-void main()
-{ float a;
-printf("\n enter a no.");
-scanf("%d",&a);
-printf("\n log %f",log(a));
-printf("\n sine %f",sin(a));
-printf("\n square %f",square(a));
-}
